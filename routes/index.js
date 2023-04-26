1b6a3f052be0f6eac76eb80dc1b9f4b71b037c75
Lucas Vandermaarel <lukevdm64@gmail.com>
2023-04-26 15:41:35 -0400

    pulling app requests correctly now

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,31 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
+var data = '';
 
-router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: 'AMDSB Application Approvals',
-        page: false,
-        isAuthenticated: req.session.isAuthenticated,
-        name: req.session.account?.name,
-    });
+var options = {
+  'method': 'GET',
+  'headers': {
+    'Authorization': 'Basic THVjYXMuVmFuZGVybWFhcmVsQGVkLmFtZHNiLmNhOndhNnF6LTI3Ymo2LXhpb2ptLXJ1N2R0LXV6Ym40'
+  }
+};
+const req = https.request('https://amdsb.topdesk.net/tas/api/incidents?query=category.name==(\'App Approval Request\')', options, (res) => {
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    data = JSON.parse(data);
+  });
+});
+
+req.end();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'TopDesk App Approval Request Helper', tickets: data }); 
 });
 
 module.exports = router;
